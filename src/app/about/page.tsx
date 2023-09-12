146352cdd47da69bde0bba4d6bfda45968369a64
Cristian Morales <cristianmorales3006@gmail.com>
2023-09-11 20:26:59 -0700

    Updated AboutUs Page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
-import ImageUs from '../../img/ImageUs.svg';
+import AboutUs from '../../img/AboutUs.svg';
 
 function AboutPage() {
   return (
-    <div className="sm:flex h-[calc(100vh-8rem)] justify-center items-center max-w-screen-xl">
-      <div className="sm:w-1/2 p-10">
-        <div className="image object-center text-center">
-          <Image
-            src={ImageUs}
-            alt="About Us Image"
-            width={400}
-            height={300}
-          />
-        </div>
-      </div>
-      <div className="sm:w-1/2 p-5">
+    <div className='flex flex-col-reverse sm:flex-row items-center'>
+      <div className='sm:w-1/2 p-5 sm:p-10 object-center'>
         <div className="text">
           <span className="text-gray-500 border-b-2 border-indigo-600 uppercase">About us</span>
           <h2 className="my-4 font-bold text-3xl sm:text-4xl ">About <span className="text-violet-400">Our Company</span></h2>
@@ -23,6 +13,9 @@ function AboutPage() {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, commodi doloremque, fugiat illum magni minus nisi nulla numquam obcaecati placeat quia, repellat tempore voluptatum.
           </p>
         </div>
+      </div>
+      <div className='sm:w-1/2 p-5 sm:p-10 object-center'>
+        <Image src={AboutUs} alt='Abouts Image' width={500} height={400} />
       </div>
     </div>
   );
